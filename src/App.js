01eeffce508108e9e938178991fa39c0af7f8008
luanaforte <luanaforte950@gmail.com>
2024-08-31 15:12:27 -0300

    Adicionando my-app-tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [task, setTasks] = useState([])
+
+  const [newTasks, setNewTasks] = useState('')
+
+  const addTask = () => {
+    if(newTasks.trim() !=="") {
+      setTasks([...task, newTasks])
+      setNewTasks('')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Lista de tarefas</h1>
+
+      <input 
+        typeof='text'
+        value={newTasks}
+        onChange={(e) => setNewTasks(e.target.value)} 
+        placeholder='Adicione uma tarefa'
+      />
+
+      <button onClick={addTask}>Adicionar</button>
+
+      <ul>
+        {task.map((task,index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
